021caceea671a4455276986bf85dd1f1c6783bf1
Theresa Tran <theresa1194@gmail.com>
2023-11-04 23:52:50 +1100

    done

--- main.js
+++ main.js
@@ -1,12 +1,36 @@
 // Defining text characters for the empty and full hearts for you to use later.
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
+let EMPTY_HEART = '♡'
+let FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+const modal = document.querySelector('#modal');
+modal.classList.add('hidden');
 
+const hearts = document.querySelectorAll('.like-glyph')
 
+for( let heart of hearts) {
+  heart.addEventListener('click', like)
+}
 
+function like(event) {
+  if (event.target.innerText === EMPTY_HEART) {
+    mimicServerCall()
+      .then(() => {
+        event.target.innerText = FULL_HEART;
+        event.target.classList.toggle('activated-heart');
+      })
+      .catch(error => {
+        modal.classList.toggle('hidden')
+        message.innerText = error
+        setTimeout(() => modal.classList.toggle('hidden'), 3000);
+      })
+  }
+  if (event.target.innerText === FULL_HEART) {
+    event.target.innerText = EMPTY_HEART;
+    event.target.classList.toggle('activated-heart');
+  }
+}
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
